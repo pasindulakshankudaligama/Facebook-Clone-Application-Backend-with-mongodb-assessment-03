b0cdcd900e2f01cb6171e902361b4f6372e869b1
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-26 01:47:58 +0530

    users post  method added

--- routes/users.js
+++ routes/users.js
@@ -24,5 +24,25 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    const users = await new Users({
+        firstName: req.body.firstName,
+        sureName: req.body.sureName,
+        gender: req.body.gender,
+        dateOfBirth: req.body.dateOfBirth,
+        password: req.body.password,
+        phoneNumber: req.body.phoneNumber,
+        email: req.body.email,
+    })
+
+    try {
+        const response = await users.save();
+        res.json(response)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
+
+
 
 module.exports = router
